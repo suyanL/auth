727e5783ff152de91543f28f439e142930cb54e1
suancyg <suyanl728@163.com>
2022-06-16 00:04:45 +0800

    编写脚本文件

--- ddup-auth-service/ddup-auth-service-captcha/build/build.sh
+++ ddup-auth-service/ddup-auth-service-captcha/build/build.sh
@@ -1,4 +1,4 @@
-export JENKINS_HOME="/root/.jenkins/workspace/springboot-jenkins"
+export JENKINS_HOME="/root/.jenkins/workspace/ddup-auth/ddup-auth-service-captcha"
 export JENKINS_VERSION="v0.1"
 
 echo "JENKINS_HOME:${JENKINS_HOME},JENKINS_VERSION:${JENKINS_VERSION}"
@@ -8,12 +8,12 @@ mvn clean
 mvn package
 
 echo "开始制作镜像"
-docker build -t 你的私有Harbor地址/yunlingfly/jenkins:$JENKINS_VERSION $JENKINS_HOME
+docker build -t 10.211.55.19:30002/ddup/ddup-auth-service-captcha:$JENKINS_VERSION $JENKINS_HOME
 
 echo "开始推镜像"
-docker push 你的私有Harbor地址/yunlingfly/jenkins:$JENKINS_VERSION
+docker push 10.211.55.19:30002/ddup/ddup-auth-service-captcha:$JENKINS_VERSION
 
 echo "开始运行镜像"
-kubectl apply -f $JENKINS_HOME/build/jenkins-svc.yaml
+kubectl apply -f $JENKINS_HOME/build/svc.yaml
 # 使用envsubst传递export的参数
-envsubst < $JENKINS_HOME/build/jenkins-deployment.yaml | kubectl apply -f -
+envsubst < $JENKINS_HOME/build/deployment.yaml | kubectl apply -f -
